a4f827851c50830fa05257330de53daaa122f97b
nikola <nikola.nikkolic@gmail.com>
2021-12-23 08:25:04 +0100

    #2 fixed game-handler-error

--- src/app/core/error-handlers/game-handler-error.ts
+++ src/app/core/error-handlers/game-handler-error.ts
@@ -1,7 +1,7 @@
 import { InformationDialogType } from "@app-enums";
 
 export class GameErrorHandler extends Error {
-  constructor(message: string, informationDialogType: InformationDialogType) {
-    super();
+  constructor(message: string, public informationDialogType: InformationDialogType) {
+    super(message);
   }
 }
